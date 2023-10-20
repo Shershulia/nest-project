901b6f119cb729043c2265a4734583e124c23d99
Shershulia <krol31072002@gmail.com>
2023-10-20 20:36:25 +0200

    Connect mongoDb to autorization

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,16 @@
+import NextAuth from 'next-auth'
+
+import GoogleProvider from 'next-auth/providers/google'
+import {MongoDBAdapter} from "@auth/mongodb-adapter";
+import clientPromise from "@/lib/mongodb";
+
+export default NextAuth({
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_ID,
+            clientSecret: process.env.GOOGLE_SECRET
+        }),
+    ],
+    adapter: MongoDBAdapter(clientPromise), //reuse an active connection to the database
+
+})
