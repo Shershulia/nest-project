5b611f214245976840b68542291dbe60bcc661ae
Shershulia <krol31072002@gmail.com>
2023-10-31 20:17:54 +0100

    Fix usage of images + add UI for editing items

--- components/adminEventPage/GetEventsForEditing.jsx
+++ components/adminEventPage/GetEventsForEditing.jsx
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import axios from "axios";
-import {Spinner} from "@/components";
+import {Spinner, Title} from "@/components";
+import Image from "next/image";
 
 const GetEventsForEditing = () => {
     const [allEvents, setAllEvents] = useState([]);
@@ -13,13 +14,27 @@ const GetEventsForEditing = () => {
 
     },[])
     return (
-        <div>
-            {isLoading && <Spinner/>}
+        <div className={"w-full"}>
+            {isLoading && <Spinner fullWidth={true}/>}
             {allEvents.length>0 && (
-                <div>{
+                <div className={"w-full border border-b-black border-x-black"}>{
                     allEvents.map((event,index) => (
-                        <div key={index}>
-                            {event.name}
+                        <div key={index} className={"w-full flex justify-between"}>
+                            <div className={"flex w-2/3 items-center justify-start gap-10"}>
+                                <Image src={event.images[0]} width={100} height={100}/>
+                                <h1 className={"text-lg font-bold"}>{event.name}</h1>
+                                <h2 className={"max-w-1/3"}>{event.description}</h2>
+                                <h2 className={"max-w-1/3"}>{event.place}</h2>
+
+                            </div>
+                            <div className={"flex w-1/3 p-6 gap-6"}>
+                                <button className={"bg-yellow-600 text-lg font-bold p-2 rounded-lg border-black w-full"}
+                                        onClick={()=>{}}>Edit</button>
+                                <button className={"bg-red-600 text-lg font-bold p-2 rounded-lg border-black w-full"}
+                                        onClick={()=>{}}>Delete</button>
+
+                            </div>
+
                         </div>
                     ))}
                 </div>
--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['nest-test-next.s3.amazonaws.com'],
+  },
 }
 
 module.exports = nextConfig
