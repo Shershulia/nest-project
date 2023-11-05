c2854be7fd64dc4dea7c463f5fa3e0de5c954d9c
Shershulia <krol31072002@gmail.com>
2023-11-05 17:14:15 +0100

    Add check permission to all pages

--- pages/admin/settings.js
+++ pages/admin/settings.js
@@ -1,14 +1,24 @@
 import React from 'react';
-import {AdminLayout} from "@/components";
+import {AdminLayout, WrongPermission} from "@/components";
+import {getAdminServerSideProps} from "@/utils/adminUtils";
 
-const SettingsAdminPage = () => {
+const SettingsAdminPage = ({isAdmin}) => {
     return (
+        isAdmin ? (
         <div className="h-full flex bg-blue-600">
             <AdminLayout>
                 <div>Admin Settings</div>
             </AdminLayout>
         </div>
-    );
+    ):
+    (
+        <WrongPermission/>
+    ));
 };
 
 export default SettingsAdminPage;
+
+//Check of admin permission happens on server side
+export async function getServerSideProps(ctx){
+    return await getAdminServerSideProps(ctx);
+}
