7472d8b5a3d9b57db568b146f747bd227c59adc3
Shershulia <krol31072002@gmail.com>
2023-11-08 02:28:20 +0100

    Fix event Event edit row

--- components/adminEventPage/EventEditRow.jsx
+++ components/adminEventPage/EventEditRow.jsx
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import Image from "next/image";
 import {EventForm} from "@/components";
+import {format} from "date-fns";
 
 const EventEditRow = ({event,deleteEvent}) => {
     const [editMode, setEditMode] = useState(false);
@@ -11,11 +12,15 @@ const EventEditRow = ({event,deleteEvent}) => {
             {!editMode && (
                 <>
                     <div className={"flex w-2/3 items-center justify-start gap-10"}>
-                        {images.length>0 ? (<Image src={event.images[0]} width={100} height={100} className={"ml-2"} alt={"Image for event " + name}/>) :
-                            (<div className={"h-[100px] w-[100px]"}></div>)}
-                        <h1 className={"text-lg font-bold"}>{event.name}</h1>
-                        <h2 className={"max-w-1/3"}>{event.description}</h2>
-                        <h2 className={"max-w-1/3"}>{event.place}</h2>
+                        {images.length>0 ? (
+                                <div className={"h-[100px] w-[100px] ml-2"}>
+                                    <img src={event.images[0]} alt={"Uploaded image"} className={"rounded-lg h-full w-full object-cover"}/>
+                                </div>
+                            ) :
+                            (<div className={"h-[100px] w-[100px] ml-2"}></div>)}
+                        <h1 className={"text-lg font-bold w-[50px]"}>{event.name}</h1>
+                        <p className={"w-[50px] truncate"}>{event.description}</p>
+                        <p className={"max-w-1/3"}>{format(new Date(event.date), 'MMMM do yyyy hh:mm a')}</p>
                     </div>
                     <div className={"flex w-1/3 p-6 gap-6"}>
                         <button className={"bg-yellow-600 text-lg font-bold p-2 rounded-lg border-black w-full"}
