94a78968d9b39c80ca4ed34a0527edd524b637f7
Ivan Shirshikov <ivansh@stud.ntnu.no>
2023-12-04 17:42:08 +0100

    GUI for google drive upload

--- components/adminDocumentsPage/DocumentForm.jsx
+++ components/adminDocumentsPage/DocumentForm.jsx
@@ -16,6 +16,13 @@ const DocumentForm = ({
     const [date, setDate] = useState(existingDate ? new Date(existingDate) : new Date());
     const [documents,setDocuments] = useState(existingDocuments || []);
     const [addToGoogle,setAddToGoogle] = useState(false);
+    const [fromGoogleDrive, setFromGoogleDrive] = useState(true);
+    const getGoogleDriveFiles = async ()=>{
+        if (fromGoogleDrive){
+
+        }
+        setFromGoogleDrive(prevState => !prevState)
+    }
 
     const createAndUploadFile = async () => {
         try {
@@ -126,7 +133,7 @@ const DocumentForm = ({
                 <Title text={"Create meeting document"}/>
                 <Input label={"Title of the meeting"} value={title} onChange={setTitle} className={'rounded-md w-1/2'}></Input>
                 <TextArea label={"Write in some extra details about the meeting"} value={description} onChange={setDescription} className={'rounded-md w-1/2'}></TextArea>
-                <div className={"flex gap-6 justify-center items-center"}>
+                <div className={"flex gap-6 flex-col justify-center items-center"}>
                     <TimePicker label={"Select the date of the meeting"} value={date} setValue={setDate} />
                     <div>
                         <Switcher title={"Add to Google Drive"} value={addToGoogle} setValue={setAddToGoogle}/>
@@ -134,19 +141,34 @@ const DocumentForm = ({
 
                 </div>
 
-                <div className={"flex flex-col justify-center gap-4 items-center"}>
+                <div className={"flex flex-col justify-center items-center"}>
                     <Title text={"Select documents"}/>
-                    <div className={"flex justify-center items-center"}>
-                        <ImageUploadComponent images={documents} setImages={setDocuments} isDocuments={true} isTitle={false}/>
-                        <h1 className={"mx-2 font-bold"}>OR </h1>
-                        <div className={"mb-2"}>
-                            <button className={"p-2 rounded-md relative border h-24 w-24 bg-gray-200"} onClick={saveToDrive}>
-                                <p className={"text-xs font-bold text-center absolute bottom-0"}>Upload using</p>
-                                <img src={"/Google_Drive.png"} alt={"Google Drive logo"}/>
-                            </button>
-                        </div>
+                    {
+                        fromGoogleDrive ? (
+                            <div className={"flex justify-center items-center"}>
+                            <ImageUploadComponent images={documents} setImages={setDocuments} isDocuments={true} isTitle={false}/>
+                            <h1 className={"mx-2 font-bold"}>OR </h1>
+                            <div className={"mb-2"}>
+                                <button className={"p-2 rounded-md relative border h-24 w-24 bg-gray-200"} onClick={getGoogleDriveFiles}>
+                                    <p className={"text-xs font-bold text-center absolute bottom-0"}>Upload using</p>
+                                    <img src={"/Google_Drive.png"} alt={"Google Drive logo"}/>
+                                </button>
+                            </div>
+                        </div>) : (
+                            <div className={"flex justify-center items-center"}>
+                                <div className={"mb-2"}>
+                                    <button className={"p-2 rounded-md  border bg-gray-200 flex items-center justify-center"} onClick={getGoogleDriveFiles}>
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+                                            <path strokeLinecap="round" strokeLinejoin="round" d="M9 8.25H7.5a2.25 2.25 0 00-2.25 2.25v9a2.25 2.25 0 002.25 2.25h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25H15m0-3l-3-3m0 0l-3 3m3-3V15" />
+                                        </svg>
+                                        <p className={"text-xs font-bold text-center bottom-0"}>Upload using local files</p>
 
-                    </div>
+                                    </button>
+                                </div>
+                            </div>
+                        )
+                    }
+
                 </div>
             </div>
             <div className={"flex items-center justify-center mt-6"}>
