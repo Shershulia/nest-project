160d86b4b6c7d4f3e9bce6eec9aff902f9ec845c
Manish Mannivannan <manish@hotmail.no>
2023-12-05 15:59:48 +0100

    api connection and some styling

--- pages/calendar.js
+++ pages/calendar.js
@@ -8,56 +8,42 @@ import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import interactionPlugin from "@fullcalendar/interaction";
 import timeGridPlugin from "@fullcalendar/timegrid";
+import Link from "next/link";
+import { set } from "lodash";
 
 export default function Calendar() {
   const { data: session } = useSession();
 
-  const [events] = useState([
-    {
-      id: "1",
-      title: "Avengers",
-      description: "They do be avenging",
-      image: "https://wallpapercave.com/wp/iptrxid.jpg",
-      start: "2023-12-10T14:00:00",
-      end: "2023-12-10T16:00:00",
-    },
-    {
-      id: "2",
-      title: "Thor Ragnarok",
-      description: "Death Sis goes on a killing spree",
-      image:
-        "https://www.teahub.io/photos/full/99-997464_thor-ragnarok-thor-ragnarok-poster-hd.jpg",
-      start: "2023-12-12T10:00:00",
-      end: "2023-12-12T12:30:00",
-    },
-    {
-      id: "3",
-      title: "Dragon Ball Super: Broly",
-      description: "Autistic Hulk goes on a rampage",
-      image:
-        "https://wallpapers.com/images/hd4/dragon-ball-super-broly-movie-poster-97a8oe0mgan1o2t7.jpg",
-      start: "2023-12-15T18:00:00",
-      end: "2023-12-15T20:00:00",
-    },
-    {
-      id: "4",
-      title: "Star Wars: The Force Awakens",
-      description: "Something did indeed awaken",
-      image: "https://getwallpapers.com/wallpaper/full/c/d/8/126215.jpg",
-      start: "2023-12-20T13:00:00",
-      end: "2023-12-20T15:30:00",
-    },
-  ]);
+  const [events, setEvents] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
-  const [selectedEvent, setSelectedEvent] = useState(events[0]);
+  const getEvents = () => {
+    setIsLoading(true);
+    axios.get(`/api/events`).then((res) => {
+      const modifiedEvents = res.data.map((event) => ({
+        ...event,
+        id: event._id,
+        start: event.date,
+      }));
+      setEvents(modifiedEvents);
+      setIsLoading(false);
+      setSelectedEvent(modifiedEvents[0]);
+    });
+  };
+
+  const [selectedEvent, setSelectedEvent] = useState([]);
 
   function handleEventClick(data) {
-    const clickedEvent = events.find((event) => event.id === data.event.id);
+    const clickedEvent = events.find((event) => event._id == data.event.id);
 
     if (clickedEvent) {
       setSelectedEvent(clickedEvent);
     }
   }
+
+  useEffect(() => {
+    getEvents();
+  }, []);
 
   return (
     <div className="flex flex-col h-screen overflow-hidden">
@@ -85,37 +71,48 @@ export default function Calendar() {
           <div className="flex flex-row w-full">
             <SideNav currentPage="Calendar" />
             <div className="w-3/5 h-auto">
-              <FullCalendar
-                plugins={[dayGridPlugin, interactionPlugin, timeGridPlugin]}
-                headerToolbar={{
-                  left: "prev,next today",
-                  center: "title",
-                  right: "dayGridMonth timeGridWeek",
-                }}
-                events={events}
-                nowIndicator={true}
-                editable={false}
-                droppable={true}
-                selectable={true}
-                selectMirror={true}
-                allDaySlot={false}
-                eventClick={(event) => handleEventClick(event)}
-              />
+              {!isLoading && (
+                <FullCalendar
+                  plugins={[dayGridPlugin, interactionPlugin, timeGridPlugin]}
+                  headerToolbar={{
+                    left: "prev,next today",
+                    center: "title",
+                    right: "dayGridMonth timeGridWeek",
+                  }}
+                  events={events}
+                  nowIndicator={true}
+                  editable={false}
+                  droppable={true}
+                  selectable={true}
+                  selectMirror={true}
+                  allDaySlot={false}
+                  eventClick={(data) => handleEventClick(data)}
+                />
+              )}
             </div>
             <div className="w-2/6 px-10 py-10 m-5 flex flex-col justify-starttime">
-              <img
-                src={selectedEvent.image}
-                alt={selectedEvent.title}
-                className="rounded-lg mb-5"
-              />
-              <div className="w-full bg-white text-black py-3 px-2 rounded-lg h-1/2 flex flex-col justify-between">
+              {selectedEvent.images && (
+                <img
+                  src={selectedEvent.images[0]}
+                  alt={selectedEvent.title}
+                  className="rounded-lg mb-5 h-1/3 object-fit"
+                />
+              )}
+              <div className="w-full bg-white text-black py-3 px-2 rounded-lg h-2/5 flex flex-col justify-between">
                 <div>
-                  <h1 className="text-4xl mb-3">{selectedEvent.title}</h1>
-                  <h2 className="text-lg mb-3">{selectedEvent.description}</h2>
+                  <h1 className="text-4xl mb-3 line-clamp-1">
+                    {selectedEvent.name}
+                  </h1>
+                  <h2 className="text-lg mb-3 line-clamp-4">
+                    {selectedEvent.description}
+                  </h2>
                 </div>
-                <button className="bg-customPurple hover:bg-violet-700 text-white font-bold py-2 px-4 rounded-lg w-fit mx-auto">
+                <Link
+                  href={`/events/${selectedEvent.id}`}
+                  className="bg-customPurple hover:bg-violet-700 text-white font-bold py-2 px-4 rounded-lg w-fit mx-auto"
+                >
                   View Event
-                </button>
+                </Link>
               </div>
             </div>
           </div>
