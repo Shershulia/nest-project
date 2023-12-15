91746dbf462ba6fa902187d590c9d95c56dbadc0
Ivan Shirshikov <ivansh@stud.ntnu.no>
2023-12-15 17:09:23 +0100

    Fix login page

--- components/LoginFormPage/LoginForm.jsx
+++ components/LoginFormPage/LoginForm.jsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from "react";
-import { signIn } from "next-auth/react";
+import {signIn, useSession} from "next-auth/react";
 import { gsap } from 'gsap';
 import Lottie from "lottie-react";
 import PhoneAnimation from "@/components/LoginFormPage/PhoneAnimation.json";
-import {useRouter} from "next/router";
+import {useRouter} from "next/navigation";
+
 
 const LoginForm = () => {
   const frameRate = 25;
@@ -13,6 +14,7 @@ const LoginForm = () => {
   const frameOfScrollEnd = 123;
   const durationOfDisappearAnimation = ((138 - frameOfScrollStart) / frameRate) * 1000;
   const router = useRouter();
+  const { data: session } = useSession();
   const [animationControl, setAnimationControl] = useState({
     autoplay: true,
     loop: false,
@@ -33,13 +35,15 @@ const LoginForm = () => {
     gsap.to('.lottie', { duration: 1, width: '30%', delay: 0 });
 
     setTimeout(() => {
-      signIn("google").then(res=>
-          router.push("/")
-      );
+      signIn("google")
     }, durationOfDisappearAnimation);
   };
 
   useEffect(() => {
+    if (session){
+      router.push("/");
+    }
+
     const timer = setTimeout(() => {
       setAnimationControl({
         autoplay: true,
